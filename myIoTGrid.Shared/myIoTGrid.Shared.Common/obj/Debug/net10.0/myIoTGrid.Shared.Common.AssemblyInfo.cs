653fb120027dc82e5e81137411d2d70f70e54971
FBersch <120514455+FBersch@users.noreply.github.com>
2025-12-14 19:06:44 +0100

    SQL Backup

--- myIoTGrid.Shared/myIoTGrid.Shared.Common/obj/Debug/net10.0/myIoTGrid.Shared.Common.AssemblyInfo.cs
+++ myIoTGrid.Shared/myIoTGrid.Shared.Common/obj/Debug/net10.0/myIoTGrid.Shared.Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Common DTOs, Enums, and Constants shared between Hub and Cloud")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3a634e8bba7d46f561d119e7498b327300294c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+000c5fb6a678fd0d465ef60d714ca3fdd51fd351")]
 [assembly: System.Reflection.AssemblyProductAttribute("myIoTGrid.Shared.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myIoTGrid.Shared.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
