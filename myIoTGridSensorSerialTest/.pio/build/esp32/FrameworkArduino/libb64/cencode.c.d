bfa173fc6bbf063964862431625b85c641bcd5d6
FBersch <120514455+FBersch@users.noreply.github.com>
2025-12-15 20:48:50 +0100

    Docker

--- myIoTGridSensorSerialTest/.pio/build/esp32/FrameworkArduino/libb64/cencode.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32/FrameworkArduino/libb64/cencode.c.o: \
- /Users/frankbersch/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.c \
- /Users/frankbersch/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.h
